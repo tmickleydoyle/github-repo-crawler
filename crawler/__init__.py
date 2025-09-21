d7996badc73fe7f02fecdf1417b53d743d188126
tmickleydoyle <tmickleydoyle@gmail.com>
2025-09-21 08:22:07 -0400

    feat:add statefulness

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,1 +1,21 @@
 # GitHub Crawler package
+
+from .client import GitHubClient
+from .config import get_settings
+from .domain import *
+from .search_strategy import SimpleSearchStrategy
+from .state_manager import StateManager, CrawlerState
+from .stateful_search_strategy import StatefulSearchStrategy
+from .search_space import SearchSpaceGenerator
+from .rate_limit_scheduler import RateLimitScheduler
+
+__all__ = [
+    "GitHubClient",
+    "get_settings",
+    "SimpleSearchStrategy",
+    "StateManager",
+    "CrawlerState",
+    "StatefulSearchStrategy",
+    "SearchSpaceGenerator",
+    "RateLimitScheduler"
+]
